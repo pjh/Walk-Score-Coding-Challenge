1ff6081baebc179fb04d64a4fd1fd30a86bae9dd
Peter Hornyack <pjh@cs.washington.edu>
2014-06-05 18:09:59 -0700

    Read stdin and construct both incoming and outgoing edge maps.

--- graph.py
+++ graph.py
@@ -2,6 +2,8 @@
 # nodes with exactly one input and one output edge.
 # Copyright (c) 2014 Peter Hornyack
 
+import re
+
 # How should we represent the graph once we have read in the edge list?
 # There are two basic structures for representing a graph: an adjacency
 # matrix, which uses a two-dimensional array to mark which edges are
@@ -22,6 +24,14 @@
 # sufficient to keep a map for outgoing edges and then keep track of
 # just a count for incoming edges? Either could probably work, but it
 # seems like the reduce algorithm may be faster if we keep both maps.
+#
+# Future enhancements:
+#   Create a separate class for the edge lists as well?
+#   Define better exceptions.
+
+# Assumption: node IDs contain one or more alphanumeric characters or
+# underscores.
+EDGE_RE = re.compile(r"^(?P<start>\w+)\t(?P<end>\w+)$")
 
 class Graph:
     """Class representing a directed graph. The representation kept
@@ -31,23 +41,65 @@ class Graph:
     needed for a basic directed graph.
     """
 
-    edgemap = None
+    outedges = None
+    inedges = None
 
     def __init__(self):
         return
 
-    def from_file(self, infile):
-        """Reads edge lines from the open file @infile and fills the
-        edgemap.
+    def add_edge(self, edgemap, start, end):
+        """Adds an edge from start to end in the specified edge map.
+        Duplicate edges are ignored.
         """
 
-        edgemap = dict()
+        try:
+            edgelist = edgemap[start]
+        except KeyError:
+            edgelist = list()
+
+        if end not in edgelist:
+            edgelist.append(end)
+            edgemap[start] = edgelist
+
+        return
+
+    def del_edge(self, edgemap, start, end):
+        """Removes an edge from start to end in the specified edge map.
+        If there is no edge from start to end, a KeyError will be raised.
+        """
+
+        return
+
+    def from_file(self, infile):
+        """Reads edge lines from the open file @infile and fills the
+        edge maps. Raises ValueError if an invalid line is encountered.
+        """
+
+        # For now, just reset these every time this method is called.
+        self.outedges = dict()
+        self.inedges = dict()
+
+        line = infile.readline()
+        while line:
+            edge_match = EDGE_RE.match(line)
+            if not edge_match:
+                raise ValueError("invalid input line {}".format(line))
+            start = edge_match.group('start')
+            end = edge_match.group('end')
+
+            self.add_edge(self.outedges, start, end)
+            self.add_edge(self.inedges, end, start)
+
+            line = infile.readline()
+
+        print("outedges: {}".format(self.outedges))
+        print("inedges: {}".format(self.inedges))
 
         return
 
     def to_file(self, outfile):
-        """Writes edges from the edgemap to the open writeable file
-        @outfile.
+        """Writes edges from the outgoing edge map to the open
+        writeable file @outfile.
         """
 
         return
